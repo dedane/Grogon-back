9e79415402ca45b86e8e890a443c22b064baddae
dedane <evanskeema@gmail.com>
2020-07-01 07:33:38 +0300

    security

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const multer = require('multer');
 const mongoose = require('mongoose');
+const cors = require('cors')
+
+const DriverRoute = require('./routes/driver')
+const MechanicRoute = require('./routes/mechanic')
+require('dotenv').config();
+
+mongoose.Promise = require('bluebird')
+mongoose.connect('mongodb+srv://grogon:' + process.env.MONGO_ATLAS_PW + '@cluster0.7wbz0.mongodb.net/GROGON?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+mongoose.set('UseCreateIndex',true)
+
+const conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'))
+conn.once('open',() => {
+    console.log('connected to a database')
+})

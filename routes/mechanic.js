d5d76013c48eea88404e9a10b96a2e7888aeebea
dedane <evanskeema@gmail.com>
2020-07-01 10:31:50 +0300

    'mechanicregistered'

--- routes/mechanic.js
+++ routes/mechanic.js
@@ -5,5 +5,50 @@ const multer = require('multer')
 const checkAuth = require('../middleware/check-auth')
 const Mechanic = require('../models/mechanic');
 
+router.post('/register ', (req,res,next) => {
+    Mechanic.find({ Email: req.body.Email })
+    .exec()
+    .then(Mechanic => {
+        if (Mechanic.length >= 1){
+            return res.status(409).json({
+                message: 'You already are registered mechanic'
+            })
+        }
+        else {
+            bcrypt.hash(req.body.Password, 10, (err,hash) => {
+                if (err){
+                    return res.status(500).json({
+                        error: err
+                    })
+                }
+                else{
+                    const Mechanic =  new Mechanic({
+                        _id: new mongoose.Types.ObjectId(),
+                        Email: req.body.Email,
+                        Password: hash,
+
+                        Name: req.body.Name,
+                        phonenumber: req.body.phonenumber,
+                        certificate: req.body.certificate,
+                        mechanicPic: req.body.mechanicPic 
+                    })
+                Mechanic.save()
+                .then(result => {
+                    console.log(result)
+                    res.status(201).json({
+                        message: 'Successfully Registered'
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                    res.status(500).json({
+                        error: err
+                    })
+                })
+                }
+            })
+        }
+    })
+})
 
 module.exports = router;

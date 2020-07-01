2beb34f14ea380d2f79d48880e498e22c99d84d5
dedane <evanskeema@gmail.com>
2020-07-01 06:49:20 +0300

    register driver

--- models/driver.js
+++ models/driver.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const DriverSchema = mongoose.Schema({
 
     _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
-    phonenumber: {type: Number, required: true},
-    password: {type: Character, required: true},
-    vehicleImage: {type: String, required: true},
+    Email: {type: String, required: true},
+    Name: {type: String, required: true},
+    Phonenumber: {type: Number, required: true},
+    Password: {type: Character, required: true},
+    VehicleImage: {type: String, required: true},
     PurchaseDate: {type: Date, required: true},
-    carmake: {type: String, required: true},
-    geometry: GeoSchema
+    Carmake: {type: String, required: true},
+    Geometry: GeoSchema
 })
 
 const GeoSchema = new mongoose.Schema({
--- routes/driver.js
+++ routes/driver.js
@@ -3,3 +3,67 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const multer = require('multer')
 const checkAuth = require('../middleware/check-auth')
+const Driver = require('../models/driver');
+
+
+router.post('/register', (req,res,next) => {
+    Driver.find({ Email: req.body.Email })
+    .exec()
+    .then(Driver => {
+        if (Driver.length >= 1){
+            return res.status(409).json({
+                message: 'You already registered Driver'
+            })
+        }
+        else {
+            bcrypt.hash(req.body.Password, 10, (err,hash) => {
+                if (err){
+                    return res.status(500).json({
+                        error:err
+                    })
+                }
+                else{
+                    const Driver = new Driver({
+                        _id: new mongoose.Types.ObjectId(),
+                        Email: req.body.Email,
+                        Password: hash,
+
+                        Name: req.body.Name,
+                        Phonenumber: req.body.Phonenumber,
+                        Carmake: req.body.Carmake,
+                        PurchaseDate: req.body.PurchaseDate,
+                        VehicleImage: req.body.VehicleImage
+                    })
+                Driver.save()
+                .then(result => {
+                    console.log(result)
+                    res.status(201).json({
+                        message: 'successfully Registered'
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                    res.status(500).json({
+                        eror: err
+                    })
+                })
+                }
+            })
+        }
+    })
+
+})
+
+router.get('/driverslocation', function(req,res,next){
+    Driver.geoNear(
+        {type: 'Point',
+        coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+        {maxDistance: 100000,
+        spherical: true}
+    ).then(function(driver){
+        res.send(driver)
+    })
+
+})
+
+module.exports = router;

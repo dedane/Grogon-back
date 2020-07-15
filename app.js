484e62f1602dd170501f8f3661a33282234a3ed8
dedane <evanskeema@gmail.com>
2020-07-15 16:44:30 +0300

    allow all

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(bodyparser.json())
 app.use(cors())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080/")
+    res.header("Access-Control-Allow-Origin", "*")
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-requested-with, content-Type, Accept, Authorization"

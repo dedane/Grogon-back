a674bae7cdd21d228edd35484cf18ecbe9bfa6ba
dedane <evanskeema@gmail.com>
2020-07-20 14:22:52 +0300

    credentilas acceptance

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(cors())
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://grogonmechanic.netlify.app/');
+    res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     if (req.method === 'OPTIONS') {
